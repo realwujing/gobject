0eaf0c3dbeec9fd168ffb47b2aa69f77e86de72d
yuanqiliang <yuanqiliang@uniontech.com>
2022-01-11 20:11:09 +0800

    feat: 使用 GObject 库模拟类的数据封装形式

--- pm-dlist/pm-dlist.h
+++ pm-dlist/pm-dlist.h
@@ -3,6 +3,8 @@
 #ifndef PM_DLIST_H
 #define PM_DLIST_H
  
+#include <glib-object.h>
+ 
 typedef struct _PMDListNode PMDListNode;
 struct  _PMDListNode {
         PMDListNode *prev;
@@ -12,21 +14,28 @@ struct  _PMDListNode {
  
 typedef struct _PMDList PMDList;
 struct  _PMDList {
+        GObject parent_instance;
         PMDListNode *head;
         PMDListNode *tail;
+};
+ 
+typedef struct _PMDListClass PMDListClass;
+struct _PMDListClass {
+        GObjectClass parent_class;
 };
  
 #endif
 
 /**
- * @brief 但是，由于 C 语言没有为数据类型提供自定义命名空间的功能，
- * 程序中所有的数据类型（包括函数）均处于同一个命名空间，
- * 这样数据类型便存在因为同名而撞车的可能性。
- * 为了避免这一问题，更专业一点的程序员会为数据类型名称添加一些前缀，
- * 并且通常会选择项目名称的缩写。我们可以为这种命名方式取一个名字，
- * 叫做 PT 格式，P 是项目名称缩写，T 是数据类型的名称。
- * 例如，对于一个多面体建模（Polyhedron Modeling）的项目，
- * 如果要为这个项目定义一个双向链表的数据类型，
- * 通常是先将 dlist.h 文件名修改为 pm-dlist.h
+ * @brief 使用 GObject 库模拟类的数据封装形式
+ * 但是，上述代码的确构成了一个类。在 GObject 世界里，类是两个结构体的组合，
+ * 一个是实例结构体，另一个是类结构体。例如，PMDList 是实例结构体，PMDListClass 是类结构体，
+ * 它们合起来便可以称为 PMDList 类（此处的“PMDList 类”只是一个称谓，并非是指 PMDList 实例结构体。
+ * 下文将要谈及的“GObject 类”的理解与此类似）。
+
+ *也许你会注意到，PMDList 类的实例结构体的第一个成员是 GObject 结构体，
+ *PMDList 类的类结构体的第一个成员是 GObjectClass 结构体。
+ *其实，GObject 结构体与 GObjectClass 结构体分别是 GObject 类的实例结构体与类结构体，
+ *当它们分别作为 PMDList 类的实例结构体与类结构体的第一个成员时，这意味着 PMDList 类继承自 GObject 类。
  * 
  */
